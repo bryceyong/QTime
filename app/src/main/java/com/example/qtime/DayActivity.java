72b6ce1b77cb48960b7d7d6cdb8c62956f57fa84
Bryce Jen Wei Yong <54676043+bryceyong@users.noreply.github.com>
2021-01-30 11:41:26 -0600

    Created DayActivity and WeekActivity and added buttons to route to specific pages

---
+++ app/src/main/java/com/example/qtime/DayActivity.java
@@ -0,0 +1,19 @@
+package com.example.qtime;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_day);
+
+        //set times for Monday
+
+    }
+}
--- app/src/main/java/com/example/qtime/WeekActivity.java
+++ app/src/main/java/com/example/qtime/WeekActivity.java
@@ -7,26 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class WeekActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_week);
 
-        Button button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.monday);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
 
             public void onClick(View v){
-                openWeekActivity();
+                openDayActivity();
             }
         });
-
     }
 
-    public void openWeekActivity(){
-        Intent intent = new Intent(this, WeekActivity.class);
+    public void openDayActivity(){
+        Intent intent = new Intent(this, DayActivity.class);
         startActivity(intent);
     }
 }
